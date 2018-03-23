14fa4546a79b4d0a71a7f7f8764c1de2586616b3
Anny <anny02041981@gmail.com>
2018-03-23 20:37:02 +0100

    First commit

---
+++ scripts/ang_module.js
@@ -0,0 +1,52 @@
+﻿angular.module("mainModuleApp", ["ngRoute"])
+.config(function ($routeProvider, $locationProvider) {
+
+    $locationProvider.html5Mode(true);
+
+    $routeProvider.when("/about", {
+        templateUrl: "/views/about.html"
+    });
+
+    $routeProvider.when("/tours", {
+        templateUrl: "/views/tours.html"
+    });
+
+    $routeProvider.when("/booking", {
+        templateUrl: "/views/booking.html"
+    });
+
+    $routeProvider.when("/", {
+        templateUrl: "/views/main_info.html"
+    });
+
+    $routeProvider.otherwise({
+        templateUrl: '/views/main_info.html'
+    });
+})
+.controller("mainModuleCtrl", function ($scope, $location) {
+
+    $scope.isTitleBlockVisible = true;
+
+    $scope.hideComp = function () {
+        $location.path("/");
+        $scope.showError = false;
+    }
+
+    $scope.getError = function (error) {
+        if (angular.isDefined(error)) {
+            if (error.required)
+                return "You should fill this field";
+        }
+    };
+
+    $scope.addNewOrder = function (orderDetails, isValid) {
+        if (isValid) {
+            $scope.showError = false;
+            $scope.orderDetails = orderDetails;
+            showHideTitleBlock();
+            $location.path("/");
+        } else {
+            $scope.showError = true;
+        }
+    };
+})
---
+++ scripts/main.js
@@ -0,0 +1,47 @@
+﻿window.onload = function () {
+
+    (function () {
+        let header = document.querySelector('header');
+
+        var start = Date.now();
+
+        var timer = setInterval(function () {
+            var timePassed = Date.now() - start;
+            if (timePassed > 10000) {
+                clearInterval(timer);
+                window.addEventListener('resize', handleResize);
+                return;
+            }
+
+            draw(timePassed / 100);
+        }, 40);
+
+        function draw(timePassed) {
+            var h = parseInt(getComputedStyle(header).height);
+            var w = parseInt(getComputedStyle(header).width);
+            header.style.backgroundSize = (timePassed + w) + 'px' + ' ' + (timePassed + h) + 'px';
+        };
+
+        //Resize background image
+        function handleResize() {
+            var h = parseInt(getComputedStyle(header).height);
+            var w = parseInt(getComputedStyle(header).width);
+
+            var bkGrWidth = parseInt(getComputedStyle(header).backgroundSize.split(' ')[0]);
+            var bkGrHeight = parseInt(getComputedStyle(header).backgroundSize.split(' ')[1]);
+
+            if (bkGrWidth != w || bkGrHeight != h)
+                header.style.backgroundSize = w + 'px' + ' ' + h + 'px';
+        };
+
+    })();
+
+};
+
+function showHideTitleBlock() {
+    let titleBlock = document.getElementById("titleBlock");
+    if (titleBlock.style.display != "none")
+        titleBlock.style.display = "none"
+    else
+        titleBlock.style.display = "block";
+};
